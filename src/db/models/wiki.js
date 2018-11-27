258a68ee8a2845dc442e3c486084142a2204c263
SpielDog <spieldog2@spieldogs-mbp.attlocal.net>
2018-11-26 19:59:15 -0600

    fully functionional wiki and sign-in

--- src/db/models/wiki.js
+++ src/db/models/wiki.js
@@ -3,11 +3,15 @@ module.exports = (sequelize, DataTypes) => {
   const Wiki = sequelize.define('Wiki', {
     title: DataTypes.STRING,
     description: DataTypes.STRING,
-    private: DataTypes.BOOLEAN
+    private: DataTypes.BOOLEAN,
+    userId: DataTypes.INTEGER
   }, {});
   Wiki.associate = function(models) {
     // associations can be defined here
-  
+    Wiki.belongsTo(models.User, {
+      foreignKey: "userId",
+      onDelete: "CASCADE",
+    });
   };
   return Wiki;
 };
