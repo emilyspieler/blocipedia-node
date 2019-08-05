5bef96de7689e2e6dae5956c1884e4ba60457dcc
Emily Spieler <emilyspieler1@gmail.com>
2019-08-05 12:33:10 -0500

    ignore update

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const app = require("./app");
 const http = require("http");
 
 
-const port = normalizePort(process.env.PORT || "8000");
+const port = normalizePort(process.env.PORT || "3000");
  app.set("port", port);
 
  const server = http.createServer(app);
